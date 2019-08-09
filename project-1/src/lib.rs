c4b005d14cb34f972983910957c343e88cdeab60
chen ganshen <cgs931219@outlook.com>
2019-08-09 18:28:18 +0800

    project1 part1

---
+++ project-1/src/lib.rs
@@ -0,0 +1,13 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {}
+    pub fn get(&mut self, key: String) -> Option<String> {
+        panic!()
+    }
+    pub fn remove(&mut self, key: String) {}
+}
