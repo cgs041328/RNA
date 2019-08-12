514c4bc100d3169591d1ff4786148408095bbea2
chen ganshen <cgs931219@outlook.com>
2019-08-12 13:59:56 +0800

    project1 part4

--- project-1/src/lib.rs
+++ project-1/src/lib.rs
@@ -1,13 +1,23 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore {}
+        KvStore {
+            store: HashMap::new(),
+        }
     }
 
-    pub fn set(&mut self, key: String, value: String) {}
-    pub fn get(&mut self, key: String) -> Option<String> {
-        panic!()
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
     }
-    pub fn remove(&mut self, key: String) {}
+    pub fn get(&mut self, key: String) -> Option<String> {
+        self.store.get(&key).cloned()
+    }
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
+    }
 }
