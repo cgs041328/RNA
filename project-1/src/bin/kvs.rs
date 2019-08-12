7890c155e5e1f855fa91e6450d01a215a3bced46
chen ganshen <cgs931219@outlook.com>
2019-08-12 12:22:13 +0800

    project1 part2

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,3 +1,42 @@
+use clap::{App, Arg, SubCommand};
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("kvs")
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand(
+            SubCommand::with_name("set")
+                .args(&[
+                    Arg::with_name("key").help("A string key").required(true),
+                    Arg::with_name("value").help("A string key").required(true),
+                ])
+                .about("Set the value of a string key to a string"),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .arg(Arg::with_name("key").help("A string key").required(true))
+                .about("Get the string value of a given string key"),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .arg(Arg::with_name("key").help("A string key").required(true))
+                .about("Remove a given key"),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("set", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("get", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
 }
